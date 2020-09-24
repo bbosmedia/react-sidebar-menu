3f4506264719ee672ff8e9016210e2e4d5fb49dd
bbosmedia <bbosmedia@users.noreply.github.com>
2020-09-24 08:20:05 +0500

    React Icons added

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+	return (
+		<div className="navabr">
+			<Link to="/"></Link>
+		</div>
+	);
+}
+
+export default Navbar;
